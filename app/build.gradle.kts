320597f3c17b9ecadf8239dc0b5edf712b029b77
chpo <chpomob@gmail.com>
2026-04-20 20:59:29 +0200

    build: target Android 15 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.calendaradd"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.calendaradd"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 2
         versionName = "1.0-RECOVERY"
 
